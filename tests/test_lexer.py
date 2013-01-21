6305b59b8865186b2f2edc8f3cc916fc8f66a65c
Thom Neale <twneale@gmail.com>
2013-01-21 14:59:45 -0500

    added token and lexer tests

---
+++ tests/test_lexer.py
@@ -0,0 +1,34 @@
+import re
+import unittest
+
+from tater.tokentype import Token as t
+from tater.core import Rule as r
+from tater.core import RegexLexer
+
+
+class TestLexer(RegexLexer):
+    """Test tuple state transitions including #pop."""
+    DEBUG = False
+    re_skip = re.compile('\s+')
+    tokendefs = {
+        'root': [
+            r(t.Root, 'a', push='rag'),
+            r(t.Root, 'e'),
+        ],
+        'beer': [
+            r(t.Beer, 'd', pop=2),
+        ],
+        'rag': [
+            r(t.Rag, 'b', push='rag'),
+            r(t.Rag, 'c', swap='beer'),
+        ],
+    }
+
+
+class TupleTransTest(unittest.TestCase):
+    def test(self):
+        lx = TestLexer()
+        toks = list(lx.tokenize('abcde'))
+        self.assertEqual(toks,
+           [(0, t.Root, 'a'), (1, t.Rag, 'b'), (2, t.Rag, 'c'),
+            (3, t.Beer, 'd'), (4, t.Root, 'e')])
---
+++ tests/test_token.py
@@ -0,0 +1,51 @@
+"""
+From pygments: https://bitbucket.org/birkenfeld/pygments-main/
+
+Copyright (c) 2006-2012 by the respective authors (see AUTHORS file).
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+* Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+import unittest
+
+from tater.tokentype import _TokenType, Token as t, is_token_subtype
+
+
+class TokenTest(unittest.TestCase):
+
+    def test_tokentype(self):
+        e = self.assertEqual
+
+        e(t.Cephalopod.Squid.Humboldt.split(),
+          [t, t.Cephalopod,
+           t.Cephalopod.Squid,
+           t.Cephalopod.Squid.Humboldt])
+
+        e(type(t.Cephalopod.Squid.Humboldt), _TokenType)
+
+    def test_functions(self):
+        self.assertTrue(is_token_subtype(t.String, t.String))
+        self.assertTrue(is_token_subtype(t.Carb.Tater, t.Carb))
+        self.assertFalse(is_token_subtype(t.Literal, t.String))
