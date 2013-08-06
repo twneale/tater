70208dbd0c36e32a0b76f8561158c236b2ad7d58
twneale <twneale@gmail.com>
2013-08-06 15:59:32 -0400

    Import matches_subtypes into top level

--- tater/__init__.py
+++ tater/__init__.py
@@ -1,6 +1,6 @@
 from lexer import RegexLexer, include
 from tokentype import Token
 from core import Rule, bygroups
-from node import matches, Node
+from node import Node, matches, matches_subtypes
 from parser import parse
 from visitor import Visitor, Transformer
