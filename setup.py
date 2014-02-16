78e705396fa43c12a43f8b74c98fe513b2ac1621
twneale <twneale@gmail.com>
2014-02-15 23:30:52 -0500

    setup.py tweak

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 long_description = """Tokenizer and basic smart-objects
 AST-builder implementation.
 """
 
 appname = "tater"
-version = "0.02"
+version = "0.04"
 
 setup(**{
     "name": appname,
@@ -16,6 +16,13 @@ setup(**{
         'tater',
         ],
     "author": "Thom Neale",
+    "packages": find_packages(exclude=['tests*']),
+    "package_data": {
+        'tater.base': ['*.py'],
+        'tater.core': ['*.py'],
+        'tater.ext': ['*.py'],
+        'tater.utils': ['*.py'],
+        },
     "author_email": "twneale@gmail.com",
     "long_description": long_description,
     "description": 'Basic tokenizer and smart-objects implentation.',
