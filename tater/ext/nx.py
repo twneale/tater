6dd2f8ce934ac1a1db3132dfd9f7811656fe8fb3
twneale <twneale@gmail.com>
2014-03-08 14:18:45 -0500

    digraph conversion

--- tater/ext/nx.py
+++ tater/ext/nx.py
@@ -1,6 +1,36 @@
 import networkx as nx
 
-from tater import Node, Visitor
+from tater import Node, Visitor, IteratorVisitor
+
+
+class DiGraphVisitor(Visitor):
+
+    def __init__(self, G):
+        self.G = G
+
+    def get_children(self, node):
+        return self.G.successors(node)
+
+    def finalize(self):
+        '''Final steps the visitor needs to take, plus the
+        return value or .visit, if any.
+        '''
+        return self
+
+
+class DiGraphIteratorVisitor(IteratorVisitor):
+
+    def __init__(self, G):
+        self.G = G
+
+    def get_children(self, node):
+        return self.G[node]
+
+    def finalize(self):
+        '''Final steps the visitor needs to take, plus the
+        return value or .visit, if any.
+        '''
+        return self
 
 
 class DiGraphRenderer(Visitor):
@@ -16,13 +46,16 @@ class DiGraphRenderer(Visitor):
         for node in nodes:
             node_id = self.get_node_id(node)
             if node_id not in G.node:
-                G.add_node(node_id, **node)
+                nx_node = node.to_data()
+                del nx_node['children']
+                G.add_node(node_id, nx_node)
 
     def generic_visit(self, node):
+        self.maybe_add_nodes(node)
         parent = getattr(node, 'parent', None)
         if parent is None:
             return
-        self.maybe_add_nodes(node, parent)
+        self.maybe_add_nodes(parent)
         parent_id = self.get_node_id(parent)
         node_id = self.get_node_id(node)
         self.G.add_edge(parent_id, node_id)
@@ -37,9 +70,10 @@ def to_digraph(root):
 
 class DiGraphLoader(object):
 
-    def __init__(self, G, root_id):
+    def __init__(self, G, root_id, node_cls=None):
+        self.node_cls = node_cls or Node
         self.G = G
-        self.root = Node(G.node[root_id])
+        self.root = self.node_cls(G.node[root_id])
         self.seen = {root_id: self.root}
 
     def build(self):
@@ -53,7 +87,7 @@ class DiGraphLoader(object):
         if node_id in self.seen:
             return
         node = self.G.node[node_id]
-        node = Node(**node)
+        node = self.node_cls.fromdata(node)
         self.seen[node_id] = node
 
     def add_edge(self, src, dest):
@@ -62,5 +96,5 @@ class DiGraphLoader(object):
         src.append(dest)
 
 
-def from_digraph(G, root_id):
-    return DiGraphLoader(G, root_id).build()
+def from_digraph(G, root_id, node_cls=None):
+    return DiGraphLoader(G, root_id, node_cls).build()
